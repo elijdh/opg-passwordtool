66fd2c0e100cd79d7853341840e6462419182096
elijdh <150734298+elijdh@users.noreply.github.com>
2024-11-07 11:00:15 -0500

    cleanup

--- passwordTool/MainWindow.xaml.cs
+++ passwordTool/MainWindow.xaml.cs
@@ -1,19 +1,7 @@
 ﻿using passwordTool.Core;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace passwordTool
 {
@@ -23,7 +11,6 @@ namespace passwordTool
     public partial class MainWindow : Window
     {
 
-        private weakEventHandler<MainWindow> _weakHandler;
         public MainWindow()
         {
 
